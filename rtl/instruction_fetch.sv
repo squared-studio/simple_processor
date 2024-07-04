a92fc4460e584a73993429b7442fa0055b39b3c2
Md Abdullah Al Samad <mdsam.raian@gmail.com>
2024-07-04 22:37:57 +0600

    adding lost program counter inside IF

--- rtl/instruction_fetch.sv
+++ rtl/instruction_fetch.sv
@@ -7,19 +7,29 @@ Author : Md Abdullah Al Samad (mdsam.raian@gmail.com)
 module instruction_fetch
   import sp_pkg::*;
 (
+  input clk_i,
+  input arst_ni,
+
+  input valid_i,
   input logic imem_ack_i,
-  input logic [ADDR_WIDTH-1:0] pc_out_i,
+
   input logic [DATA_WIDTH-1:0] imem_rdata_i,
   output logic imem_req_o,
-  output logic [ADDR_WIDTH-1:0] pc_out_o,
+  output logic [ADDR_WIDTH-1:0] pc_out,
   output logic [DATA_WIDTH-1:0] instruction_o
 );
 
   //////////////////////////////////////////////////////////////////////////////////////////////////
+  //-PROCEDURAL
+  //////////////////////////////////////////////////////////////////////////////////////////////////
+  always_ff @(posedge clk_i or negedge arst_ni) begin
+    if(arst_ni==0) pc_out = '0;
+    else if (valid_i==1) pc_out = pc_out+2;
+  end
+  //////////////////////////////////////////////////////////////////////////////////////////////////
   //-ASSIGNMENTS
   //////////////////////////////////////////////////////////////////////////////////////////////////
   assign imem_req_o = 1;
-  assign pc_out_o = pc_out_i;
   assign instruction_o = (imem_ack_i)?imem_rdata_i:'0;
 
   //////////////////////////////////////////////////////////////////////////////////////////////////
