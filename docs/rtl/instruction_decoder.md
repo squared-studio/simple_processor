3cb2e2665df81a6aa2a1b12ef90b0dee630a51a6
Foez Ahmed <foez.official@gmail.com>
2024-07-04 10:20:58 +0600

    modified:   docs/rtl/instruction_decoder.md modified:   docs/rtl/instruction_decoder_top.svg deleted:    rtl/execution_unit.sv modified:   rtl/instruction_decoder.sv modified:   rtl/reg_file.sv

--- docs/rtl/instruction_decoder.md
+++ docs/rtl/instruction_decoder.md
@@ -1,12 +1,13 @@
 # instruction_decoder (module)
 
-### Author : 
+### Author : Shahid Uddin Ahmed (shahidshakib0@gmail.com)
 
 ## TOP IO
 <img src="./instruction_decoder_top.svg">
 
 ## Description
 
+This module separates out the different register address and function to execute with them
 
 ## Parameters
 |Name|Type|Dimension|Default Value|Description|
@@ -15,10 +16,10 @@
 ## Ports
 |Name|Direction|Type|Dimension|Description|
 |-|-|-|-|-|
-|code_i|input|logic [ ILEN-1:0]|||
-|func_valid_o|output|logic|||
-|func_o|output|func_t|||
-|rd_o|output|logic [REG_ADDR_WIDTH-1:0]|||
-|rs1_o|output|logic [REG_ADDR_WIDTH-1:0]|||
-|rs2_o|output|logic [REG_ADDR_WIDTH-1:0]|||
-|imm_o|output|logic [ XLEN-1:0]|||
+|code_i|input|logic [ILEN-1:0]|| INPUT INSTRUCTUIN CODE, ILEN = 16 BIT|
+|func_valid_o|output|logic|| VALID, 0 OR 1|
+|func_o|output|func_t|| OPPCODE, ENUM TYPEDEF|
+|rd_o|output|logic [REG_ADDR_WIDTH-1:0]|| DESTINATION REGISTER, REG_ADD_WIDTH = 3 BIT|
+|rs1_o|output|logic [REG_ADDR_WIDTH-1:0]|| SOURCE REGISTER 1|
+|rs2_o|output|logic [REG_ADDR_WIDTH-1:0]|| SOURCE REGISTER 2|
+|imm_o|output|logic [ XLEN-1:0]|| IMMEDIATE VALUE, XLEN = 32 BIT|
--- rtl/instruction_decoder.sv
+++ rtl/instruction_decoder.sv
@@ -1,103 +1,141 @@
 /*
-Write a markdown documentation for this systemverilog module:
+This module separates out the different register address and function to execute with them
 Author : Shahid Uddin Ahmed (shahidshakib0@gmail.com)
 */
 
-  //////////////////////////////////////////////////////////////////////////////////////////////////
-  //-LOCALPARAMS GENERATED
-  //////////////////////////////////////////////////////////////////////////////////////////////////
+`include "sp_pkg.sv"
 
-  //////////////////////////////////////////////////////////////////////////////////////////////////
-  //-TYPEDEFS
-  //////////////////////////////////////////////////////////////////////////////////////////////////
-  
-  //////////////////////////////////////////////////////////////////////////////////////////////////
-  //-SIGNALS
-  //////////////////////////////////////////////////////////////////////////////////////////////////
-
-`include "sp_pkg.sv"//INCLUDE BECAUSE NO FLIST PRESENT
 module instruction_decoder
-  import sp_pkg::*;//-PACKAGES//-PARAMETERS //-LOCALPARAMS
+  import sp_pkg::*;
 (
-    input  logic  [          ILEN-1:0] code_i,// INPUT INSTRUCTUIN CODE, ILEN = 16 BIT
-    output logic                       func_valid_o,// VALID, 0 OR 1
-    output func_t                      func_o,//OPPCODE, ENUM TYPEDEF 
-    output logic  [REG_ADDR_WIDTH-1:0] rd_o,//DESTINATION REGISTER, REG_ADD_WIDTH = 3 BIT
-    output logic  [REG_ADDR_WIDTH-1:0] rs1_o,// SOURCE REGISTER 1
-    output logic  [REG_ADDR_WIDTH-1:0] rs2_o,//SOURCE REGISTER 2
-    output logic  [          XLEN-1:0] imm_o//IMMEDIATE VALUE, XLEN = 32 BIT
+    // INPUT INSTRUCTUIN CODE, ILEN = 16 BIT
+    input logic [ILEN-1:0] code_i,
+
+    // VALID, 0 OR 1
+    output logic                       func_valid_o,
+    // OPPCODE, ENUM TYPEDEF
+    output func_t                      func_o,
+    // DESTINATION REGISTER, REG_ADD_WIDTH = 3 BIT
+    output logic  [REG_ADDR_WIDTH-1:0] rd_o,
+    // SOURCE REGISTER 1
+    output logic  [REG_ADDR_WIDTH-1:0] rs1_o,
+    // SOURCE REGISTER 2
+    output logic  [REG_ADDR_WIDTH-1:0] rs2_o,
+    // IMMEDIATE VALUE, XLEN = 32 BIT
+    output logic  [          XLEN-1:0] imm_o
 );
+
   //////////////////////////////////////////////////////////////////////////////////////////////////
   //-ASSIGNMENTS
   //////////////////////////////////////////////////////////////////////////////////////////////////
 
-  assign rd_o = code_i[15:13];//ADDRESS BIT 13 TO 15 FOR DESTINATION REGISTER
-  assign rs1_o = code_i[12:10];//ADDRESS BIT 10 TO 12 FOR SOURCE REGISTER 1
-  assign rs2_o = code_i[9:7];//ADDRESS BIT 7 TO 9 FOR SOURCE REGISTER 2
-  assign imm_o[5:0] = code_i[9:4];//ADDRESS BIT 4 TO 9 FOR IMMEDIATE VALUE
-  assign imm_o[31:6] = code_i[9] ? '1 : '0;//REMAIN 26 BIT WILL BE FILLED WITH THE MSB OF IMMEDIATE VALUE
-  
-  //////////////////////////////////////////////////////////////////////////////////////////////////
-  //-RTLS
-  //////////////////////////////////////////////////////////////////////////////////////////////////
-  
-  //////////////////////////////////////////////////////////////////////////////////////////////////
-  //-METHODS
-  //////////////////////////////////////////////////////////////////////////////////////////////////
-  
-  //////////////////////////////////////////////////////////////////////////////////////////////////
-  //-SEQUENTIALS
-  //////////////////////////////////////////////////////////////////////////////////////////////////
+  // Address bit 13 to 15 for destination register
+  assign rd_o = code_i[15:13];
+
+  // Address bit 10 to 12 for source register 1
+  assign rs1_o = code_i[12:10];
+
+  // Address bit 7 to 9 for source register 2
+  assign rs2_o = code_i[9:7];
+
+  // Address bit 4 to 9 for immediate value
+  assign imm_o[5:0] = code_i[9:4];
+
+  // Remain 26 bit will be filled with the MSB of immediate value
+  assign imm_o[31:6] = code_i[9] ? '1 : '0;
 
   //////////////////////////////////////////////////////////////////////////////////////////////////
-  //-INITIAL CHECKS
+  //-ASSIGNMENTS
   //////////////////////////////////////////////////////////////////////////////////////////////////
 
-  //////////////////////////////////////////////////////////////////////////////////////////////////
-  //-COMBINATIONAL
-  //////////////////////////////////////////////////////////////////////////////////////////////////
+  // deciding the func_o and func_valid_o
+  always_comb begin
+    case (code_i[3:0])
 
-  always_comb begin//COMBINATIONAL BLOCK
-    case (code_i[3:0])//CASE WHERE OPCODE 
-      default: begin func_o = INVAL; func_valid_o = '0; end//VALID = 0 FOR DEFAULT VALUE OR INVALID VALUE 
-      'b0001:  begin func_o = ADDI ; func_valid_o = '1; end//RD = RS1 + IMM
-      'b0011:  begin func_o = ADD  ; func_valid_o = '1; end//RD = RS1 + RS2
-      'b1011:  begin func_o = SUB  ; func_valid_o = '1; end//RD = RS1 - RS2
-      'b0101:  begin func_o = AND  ; func_valid_o = '1; end//RD = RS1 & RS2
-      'b1101:  begin func_o = OR   ; func_valid_o = '1; end//RD = RS1 | RS2
-      'b1111:  begin func_o = XOR  ; func_valid_o = '1; end//RD = RS1 ^ RS2
-      'b0111:  begin func_o = NOT  ; func_valid_o = '1; end//RD = ~RS1
-      'b0010:  begin func_o = LOAD ; func_valid_o = '1; end//RD = mem[RS1]
-      'b1010:  begin func_o = STORE; func_valid_o = '1; end//mem[RS1] = RS2
-      'b0110:  begin func_o = SLL  ; func_valid_o = '1; end//RD = RS1 >> RS2
-      'b0100:  begin func_o = SLR  ; func_valid_o = '1; end//RD = RS1 << RS2
-      'b1110:  begin func_o = SLLI ; func_valid_o = '1; end//RD = RS1 >> IMM
-      'b1100:  begin func_o = SLRI ; func_valid_o = '1; end//RD = RS1 << IMM
+      //VALID = 0 FOR DEFAULT VALUE OR INVALID VALUE
+      default: begin
+        func_o = INVAL;
+        func_valid_o = '0;
+      end
+
+      //RD = RS1 + IMM
+      'b0001: begin
+        func_o = ADDI;
+        func_valid_o = '1;
+      end
+
+      //RD = RS1 + RS2
+      'b0011: begin
+        func_o = ADD;
+        func_valid_o = '1;
+      end
+
+      //RD = RS1 - RS2
+      'b1011: begin
+        func_o = SUB;
+        func_valid_o = '1;
+      end
+
+      //RD = RS1 & RS2
+      'b0101: begin
+        func_o = AND;
+        func_valid_o = '1;
+      end
+
+      //RD = RS1 | RS2
+      'b1101: begin
+        func_o = OR   ;
+        func_valid_o = '1;
+      end
+
+      //RD = RS1 ^ RS2
+      'b1111: begin
+        func_o = XOR;
+        func_valid_o = '1;
+      end
+
+      //RD = ~RS1
+      'b0111: begin
+        func_o = NOT;
+        func_valid_o = '1;
+      end
+
+      //RD = mem[RS1]
+      'b0010: begin
+        func_o = LOAD;
+        func_valid_o = '1;
+      end
+
+      //mem[RS1] = RS2
+      'b1010: begin
+        func_o = STORE;
+        func_valid_o = '1;
+      end
+
+      //RD = RS1 >> RS2
+      'b0110: begin
+        func_o = SLL;
+        func_valid_o = '1;
+      end
+
+      //RD = RS1 << RS2
+      'b0100: begin
+        func_o = SLR;
+        func_valid_o = '1;
+      end
+
+      //RD = RS1 >> IMM
+      'b1110: begin
+        func_o = SLLI;
+        func_valid_o = '1;
+      end
+
+      //RD = RS1 << IMM
+      'b1100: begin
+        func_o = SLRI;
+        func_valid_o = '1;
+      end
     endcase
   end
-  
-//   `ifdef SIMULATION
-//   initial begin
-//     if (DATA_WIDTH > 2) begin
-//       $display("\033[1;33m%m DATA_WIDTH\033[0m");
-//     end
-//   end
-// `endif  // SIMULATION
 
 endmodule
-
-
-
-
-  
-  
-  
-
-  
-
-  
-
-  
-
-
-
